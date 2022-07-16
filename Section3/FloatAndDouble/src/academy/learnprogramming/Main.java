c95c07f4770690cf64a7147568a91b90096a8faa
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-16 21:10:11 +0300

    S3: Floating Point Precision and a Challenge

--- Section3/FloatAndDouble/src/academy/learnprogramming/Main.java
+++ Section3/FloatAndDouble/src/academy/learnprogramming/Main.java
@@ -13,9 +13,20 @@ public class Main {
         System.out.println("Double minimum value = " + myMinDoubleValue);
         System.out.println("Double maximum value = " + myMaxDoubleValue);
 
-        int myIntValue = 5;
-        float myFloatNumber = 5.25f;
-        double myDoubleNumber = 5.25d;
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3;
+        double myDoubleValue = 5.0  / 3.0;
+        System.out.println("MyIntValue = " + myIntValue);
+        System.out.println("MyFloatValue = " + myFloatValue);
+        System.out.println("MyDoubleValue = " + myDoubleValue);
 
+        double pounds = 50d;
+        double pounds2kilo = pounds * 0.45359237;
+        System.out.println(pounds + " pounds is: " + pounds2kilo + " kilograms");
+
+        double pi = 3.14159265;
+        double anotherNumber = 3_000_000.4_567_890d;
+        System.out.println(pi);
+        System.out.println(anotherNumber);
     }
 }
