653f0683e23eac82e7ef968a6fe5ad5a9670c803
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-16 19:50:29 +0300

    S3: Primitive Types (float and double)

--- Section3/FloatAndDouble/src/academy/learnprogramming/Main.java
+++ Section3/FloatAndDouble/src/academy/learnprogramming/Main.java
@@ -3,6 +3,19 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatNumber = 5.25f;
+        double myDoubleNumber = 5.25d;
+
     }
 }
