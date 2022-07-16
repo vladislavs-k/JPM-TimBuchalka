4feeefaeb96bb8a73dd7db7ba66622842ddc7a44
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-16 21:43:14 +0300

    S3: Primitive Data Types (char and boolean)

--- Section3/CharAndBoolean/src/academy/learnprogramming/Main.java
+++ Section3/CharAndBoolean/src/academy/learnprogramming/Main.java
@@ -3,6 +3,17 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyRightChar = '\u00a9';
+        System.out.println(myCopyRightChar);
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+
     }
 }
