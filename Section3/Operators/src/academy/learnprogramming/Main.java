063822e2c652fe0e873114e2d3412040d07e1650
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-17 16:29:16 +0300

    S3: Logical 'AND' Operator

--- Section3/Operators/src/academy/learnprogramming/Main.java
+++ Section3/Operators/src/academy/learnprogramming/Main.java
@@ -49,5 +49,15 @@ public class Main {
             System.out.println("It is not an alien!");
             System.out.println("And I am scared of aliens");
         }
+
+        int topScore = 80;
+        if (topScore < 100){
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 81;
+        if (topScore > secondTopScore && topScore < 100){
+            System.out.println("Greater than second top score and less than 100");
+        }
     }
 }
