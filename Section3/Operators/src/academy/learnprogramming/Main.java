359617b195a7602a91b28d112b978f25c158d501
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-17 16:34:04 +0300

    S3: Logical 'OR' Operator

--- Section3/Operators/src/academy/learnprogramming/Main.java
+++ Section3/Operators/src/academy/learnprogramming/Main.java
@@ -55,9 +55,13 @@ public class Main {
             System.out.println("You got the high score!");
         }
 
-        int secondTopScore = 81;
-        if (topScore > secondTopScore && topScore < 100){
+        int secondTopScore = 95;
+        if ( (topScore > secondTopScore) && (topScore < 100) ){
             System.out.println("Greater than second top score and less than 100");
+        }
+
+        if ( (topScore > 90) || (secondTopScore <= 90)){
+            System.out.println("Either or both of the conditions are true");
         }
     }
 }
