fe3e1dbe22cad776d441d28b196c2c9b7050384e
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-19 01:11:33 +0300

    S4: Statements, Whitespace and Indentation (Code Organization)

--- Section4/StatementsWhiteSpaceAndIndenting/src/com/timbuchalka/Main.java
+++ Section4/StatementsWhiteSpaceAndIndenting/src/com/timbuchalka/Main.java
@@ -3,6 +3,24 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int myVariable = 50;
+
+        if (myVariable == 50) {
+            System.out.println("Printed");
+        }
+
+
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is" +
+                " another" +
+                " still more.");
+
+        int anotherVariable = 50;
+        myVariable--;
+        System.out.println("This is another one");
     }
 }
