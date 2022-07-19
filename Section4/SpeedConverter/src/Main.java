c6a40745549f5f7968a907530a533928cd257ecc
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-19 05:16:57 +0300

    S4: Coding Exercise 1: Speed Converter

---
+++ Section4/SpeedConverter/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(SpeedConverter.toMilesPerHour(1.5));
+        System.out.println(SpeedConverter.toMilesPerHour(10.25));
+        System.out.println(SpeedConverter.toMilesPerHour(-5.6));
+        System.out.println(SpeedConverter.toMilesPerHour(25.45));
+        System.out.println(SpeedConverter.toMilesPerHour(75.114));
+
+        SpeedConverter.printConversion(1.5);
+        SpeedConverter.printConversion(10.25);
+        SpeedConverter.printConversion(-5.6);
+        SpeedConverter.printConversion(25.45);
+        SpeedConverter.printConversion(75.114);
+    }
+}
