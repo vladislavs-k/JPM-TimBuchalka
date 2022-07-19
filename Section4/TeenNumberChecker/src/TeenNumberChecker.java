0db23fb70beb78802dddb49ebba1acd182415ac1
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-20 02:34:33 +0300

    S4: Coding Exercise 7: Teen Number Checker

--- Section4/TeenNumberChecker/src/TeenNumberChecker.java
+++ Section4/TeenNumberChecker/src/TeenNumberChecker.java
@@ -1,2 +1,9 @@
 public class TeenNumberChecker {
+    public static boolean hasTeen(int teenOne, int teenTwo, int teenThree){
+        return isTeen(teenOne) || isTeen(teenTwo) || isTeen(teenThree);
+    }
+
+    public static boolean isTeen(int teen){
+        return teen >= 13 && teen <= 19;
+    }
 }
