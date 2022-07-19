2500af91d2a2957de8f166d7088964237acc4a9c
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-19 23:55:17 +0300

    S4: Coding Exercise 4: Leap Year Calculator

--- Section4/LeapYearCalculator/src/LeapYear.java
+++ Section4/LeapYearCalculator/src/LeapYear.java
@@ -1,2 +1,19 @@
 public class LeapYear {
+    public static boolean isLeapYear(int year){
+
+        if ( ((year % 4) != 0) || (year < 1) || (year > 9999) ){
+            return false;
+        }
+
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+
+    }
 }
