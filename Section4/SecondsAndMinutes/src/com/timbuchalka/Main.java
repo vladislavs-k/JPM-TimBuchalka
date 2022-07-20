9735bbf14de691631a973c8febe6528e77c76c2b
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-20 04:43:13 +0300

    S4: Seconds and Minutes Challenge

--- Section4/SecondsAndMinutes/src/com/timbuchalka/Main.java
+++ Section4/SecondsAndMinutes/src/com/timbuchalka/Main.java
@@ -3,7 +3,30 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        getDurationString(61, 59);
+
+        getDurationString(3661);
     }
-    
+
+    public static void getDurationString(int minutes, int seconds){
+        if (minutes < 0 || seconds < 0 || seconds > 59){
+            System.out.println("Invalid value");
+        } else {
+            int hours = minutes / 60;
+            minutes = minutes % 60;
+
+            System.out.println(hours + "h " + minutes + "m " + seconds + "s");
+        }
+    }
+
+    public static void getDurationString(int seconds) {
+        if (seconds < 0) {
+            System.out.println("Invalid value");
+        }else {
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+            getDurationString(minutes, seconds);
+        }
+    }
+
 }
