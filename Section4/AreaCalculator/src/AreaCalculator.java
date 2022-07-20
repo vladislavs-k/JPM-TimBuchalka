495c6f6ce5bdeff7fac9cebe55449a2ce336efc5
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-20 05:39:34 +0300

    S4: Coding Exercise 8: Area Calculator

--- Section4/AreaCalculator/src/AreaCalculator.java
+++ Section4/AreaCalculator/src/AreaCalculator.java
@@ -1,2 +1,17 @@
 public class AreaCalculator {
+
+    public static double area(double radius){
+        if (radius < 0){
+            return -1d;
+        }
+        return Math.pow(radius,2) * Math.PI;
+    }
+
+    public static double area(double a, double b){
+        if (a < 0 || b < 0){
+            return -1d;
+        }
+        return a * b;
+    }
+
 }
