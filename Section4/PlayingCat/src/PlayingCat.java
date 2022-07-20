f121e378e321001838f93c726f27f445aaca8d63
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-20 06:50:13 +0300

    S4: Coding Exercise 11: Playing Cat

--- Section4/PlayingCat/src/PlayingCat.java
+++ Section4/PlayingCat/src/PlayingCat.java
@@ -1,2 +1,8 @@
 public class PlayingCat {
+    public static boolean isCatPlaying(boolean summer, int temperature){
+        if (  temperature >= 25 && ( (!summer && temperature <= 35) || (summer && temperature <= 45) ) ){
+            return true;
+        }
+        return false;
+    }
 }
