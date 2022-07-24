a8abe5d5ff10a0a70bdc9df6f1ac735471df1eab
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-24 03:01:10 +0300

    S5: Sum 3 and 5 Challenge

--- Section5/SumThreeAndFive/src/com/timbuchalka/Main.java
+++ Section5/SumThreeAndFive/src/com/timbuchalka/Main.java
@@ -22,6 +22,20 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        int sum = 0;
+        int count = 0;
+
+        for (int i = 1; i <= 1000; i++){
+            if ( (i % 3 == 0) && (i % 5 == 0) ){
+                System.out.println("The number '" + i + "' can be divided by both 3 and 5");
+                sum += i;
+                count++;
+                if (count == 5){
+                    System.out.println("The sum of all found numbers is " + sum);
+                    break;
+                }
+            }
+        }
     }
 }
