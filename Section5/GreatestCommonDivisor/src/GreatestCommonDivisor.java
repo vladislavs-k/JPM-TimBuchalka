ecf9e4242b69e7f5ecd29b0d5640dccccf7de7c3
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-25 04:43:03 +0300

    S5: Coding Exercise 20: Greatest Common Divisor

--- Section5/GreatestCommonDivisor/src/GreatestCommonDivisor.java
+++ Section5/GreatestCommonDivisor/src/GreatestCommonDivisor.java
@@ -1,2 +1,24 @@
 public class GreatestCommonDivisor {
+    public static int getGreatestCommonDivisor(int first, int second){
+        if (first < 10 || second < 10){
+            return -1;
+        }
+
+        int min;
+        if (first < second){
+            min = first;
+        }else {
+            min = second;
+        }
+
+        int divisor = 0;
+        for (int i = 1; i <= min; i++){
+            if(first % i == 0 && second % i == 0){
+                divisor = i;
+            }
+        }
+
+        return divisor;
+
+    }
 }
