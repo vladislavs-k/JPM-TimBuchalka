0dafdd4583531e675ca4170760231b76f97e0fbe
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-25 03:35:35 +0300

    S5: Coding Exercise 17: Even Digit Sum

--- Section5/EvenDigitSum/src/EvenDigitSum.java
+++ Section5/EvenDigitSum/src/EvenDigitSum.java
@@ -1,2 +1,20 @@
 public class EvenDigitSum {
+    public static int getEvenDigitSum(int number){
+        if (number < 0){
+            return -1;
+        }
+
+        int sum = 0;
+        int digit = 0;
+        while (number > 0){
+            digit = number % 10;
+            if( (digit % 2) == 0 ){
+                sum += digit;
+            }
+            number /= 10;
+        }
+        return sum;
+
+    }
+
 }
