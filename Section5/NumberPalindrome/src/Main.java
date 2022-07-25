872c17ae63d0cb0dcc8e2a35fbd9fe7734c58e63
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-25 03:06:38 +0300

    S5: Coding Exercise 15: Number Palindrome

--- Section5/NumberPalindrome/src/Main.java
+++ Section5/NumberPalindrome/src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(NumberPalindrome.isPalindrome(-1221));
+        System.out.println(NumberPalindrome.isPalindrome(707));
+        System.out.println(NumberPalindrome.isPalindrome(11212));
+        System.out.println(NumberPalindrome.isPalindrome(12321));
+
+    }
 }
