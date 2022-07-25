742814cb7fa1147241a3ae5f3e6f03f7538e3e19
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-25 06:25:21 +0300

    S5: Coding Exercise 23: Number To Words

--- Section5/NumberToWords/src/Main.java
+++ Section5/NumberToWords/src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args) {
+        NumberToWords.numberToWords(123);
+        System.out.println();
+        NumberToWords.numberToWords(1010);
+        System.out.println();
+        NumberToWords.numberToWords(1000);
+        System.out.println();
+        NumberToWords.numberToWords(-12);
+    }
 }
