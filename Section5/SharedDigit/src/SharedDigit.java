d463208495383f37a78da4bfe750efd8e8bbd1f6
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-25 03:52:16 +0300

    S5: Coding Exercise 18: Shared Digit

--- Section5/SharedDigit/src/SharedDigit.java
+++ Section5/SharedDigit/src/SharedDigit.java
@@ -1,2 +1,20 @@
 public class SharedDigit {
+    public static boolean hasSharedDigit(int numOne, int numTwo){
+        if (numOne < 10 || numOne > 99 ||numTwo < 10 || numTwo > 99){
+            return false;
+        }
+
+        int numOneSubNumOne = numOne % 10;
+        int numOneSubNumTwo = numOne / 10;
+        int numTwoSubNumOne = numTwo % 10;
+        int numTwoSubNumTwo = numTwo / 10;
+
+        if (numOneSubNumOne == numTwoSubNumOne || numOneSubNumOne == numTwoSubNumTwo ||
+            numOneSubNumTwo == numTwoSubNumOne || numOneSubNumTwo == numTwoSubNumTwo){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
 }
