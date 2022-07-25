bda2f66650b7644255369c582e983a8d0590c892
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-25 03:20:20 +0300

    S5: Coding Exercise 16: First And Last Digit Sum

--- Section5/FirstLastDigitSum/src/FirstLastDigitSum.java
+++ Section5/FirstLastDigitSum/src/FirstLastDigitSum.java
@@ -1,2 +1,17 @@
 public class FirstLastDigitSum {
+
+    public static int sumFirstAndLastDigit(int number){
+        if (number < 0){
+            return -1;
+        }
+
+        int firstNum = 0;
+        int lastNum = number % 10;
+        do {
+            firstNum = number;
+            number /= 10;
+        }while (number > 0);
+
+        return firstNum + lastNum;
+    }
 }
