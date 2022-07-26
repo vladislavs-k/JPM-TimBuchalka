07a0c2ba879e569c7eb7d92dc4a62cb8fd97de64
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-26 05:48:00 +0300

    S5: Min and Max Challenge

--- Section5/MinAndMaxScanner/src/com/timbuchalka/Main.java
+++ Section5/MinAndMaxScanner/src/com/timbuchalka/Main.java
@@ -1,8 +1,48 @@
 package com.timbuchalka;
 
+import java.util.Scanner;
+
+/*
+-Read the numbers from the console entered by the user and print the minimum and maximum number the user has entered.
+-Before the user enters the number, print the message "Enter number:"
+-If the user enters an invalid number, break out of the loop and print the minimum and maximum number.
+
+Hint:
+-Use an endless while loop.
+
+ */
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        while (true){
+
+            System.out.print("Enter number: ");
+            boolean hasAnInt = scanner.hasNextInt();
+
+            if(hasAnInt){
+
+                int number = scanner.nextInt();
+
+                if (number < min){
+                    min = number;
+                }
+                if (number > max){
+                    max = number;
+                }
+
+            } else {
+                break;
+            }
+        }
+
+        System.out.println("Min number is: " + min);
+        System.out.println("Max number is: " + max);
+
+        scanner.close();
     }
 }
