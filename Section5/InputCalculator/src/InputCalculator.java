150438f028952a637683ea6166a31ec3a3cac375
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-26 06:23:19 +0300

    S5: Coding Exercise 27: Input Calculator

--- Section5/InputCalculator/src/InputCalculator.java
+++ Section5/InputCalculator/src/InputCalculator.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class InputCalculator {
+    public static void inputThenPrintSumAndAverage(){
+        Scanner scanner =  new Scanner(System.in);
+
+        int sum = 0;
+        int count = 0;
+
+        while (true){
+            boolean isAnInt = scanner.hasNextInt();
+
+            if(isAnInt){
+                sum += scanner.nextInt();;
+                count++;
+            } else {
+                break;
+            }
+        }
+
+
+        long avg = Math.round( (double) sum / (double) count );
+        System.out.println("SUM = " + sum + " AVG = " + avg);
+
+        scanner.close();
+    }
 }
