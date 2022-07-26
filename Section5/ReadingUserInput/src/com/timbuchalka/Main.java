070aae34458290d91492c5e527dff21738940f25
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-26 04:50:09 +0300

    S5: Problems and Solutions (Reading User Input)

--- Section5/ReadingUserInput/src/com/timbuchalka/Main.java
+++ Section5/ReadingUserInput/src/com/timbuchalka/Main.java
@@ -8,14 +8,25 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter your year of birth: ");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine();
 
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        int age = 2022 - yearOfBirth;
+        boolean hasNextInt = scanner.hasNextInt();
 
-        System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+        if (hasNextInt){
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); //handle net line character (enter key)
+
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+            int age = 2022 - yearOfBirth;
+
+            if(age >= 0 && age <= 100){
+                System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+            } else {
+                System.out.println("Invalid year of birth");
+            }
+        } else{
+            System.out.println("Unable to parse year of birth");
+        }
 
         scanner.close();
     }
