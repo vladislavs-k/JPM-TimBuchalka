3ac0dd9971b85777d26124c6bd01dbde832b73b0
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-31 00:15:07 +0300

    S6: Inheritance - Part 2

--- Section6/Inheritance/src/com/timbuchalka/Main.java
+++ Section6/Inheritance/src/com/timbuchalka/Main.java
@@ -7,7 +7,10 @@ public class Main {
 
         Dog dog = new Dog("Yorkie", 8, 20,
                 2, 4 ,1, 20, "long silky");
-        dog.eat();
+//        dog.eat();
+        dog.walk();
+//        dog.run();
+
 
     }
 }
