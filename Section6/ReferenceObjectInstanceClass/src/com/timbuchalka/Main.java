fc79864082728bd92d1f51369d9253f66fed9e14
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-31 00:58:00 +0300

    S6: Reference vs Object vs Instance vs Class

--- Section6/ReferenceObjectInstanceClass/src/com/timbuchalka/Main.java
+++ Section6/ReferenceObjectInstanceClass/src/com/timbuchalka/Main.java
@@ -3,6 +3,25 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        House blueHouse = new House("blue");
+        House anotherHouse = blueHouse;
+
+        System.out.println(blueHouse.getColor());   // prints "blue"
+        System.out.println(anotherHouse.getColor());// blue
+        System.out.println();
+
+        anotherHouse.setColor("red");
+        System.out.println(blueHouse.getColor());   // red
+        System.out.println(anotherHouse.getColor());// red
+        System.out.println();
+
+        House greenHouse= new House("green");
+        anotherHouse = greenHouse;
+        System.out.println(blueHouse.getColor());   // red
+        System.out.println(greenHouse.getColor());  // green
+        System.out.println(anotherHouse.getColor());// green
+
+
+
     }
 }
