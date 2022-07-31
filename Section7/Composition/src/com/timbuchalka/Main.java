6a243a7a28f787ddcb88e0c63e96e6e112494f9f
Vladislavs K <vladislavs.kcs@gmail.com>
2022-07-31 06:04:13 +0300

    S7: Composition Part 1

--- Section7/Composition/src/com/timbuchalka/Main.java
+++ Section7/Composition/src/com/timbuchalka/Main.java
@@ -3,6 +3,21 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Dimensions dimensions = new Dimensions(20, 20, 5);
+        Case theCase = new Case("220B", "Dell",
+                "240", dimensions);
+
+        Monitor monitor = new Monitor("27in Beast", "Acer",
+                27, new Resolution(2540, 1440));
+
+        Motherboard motherboard = new Motherboard("BJ-200", "Asus",
+                4, 6, "v2.44");
+
+        PC thePC = new PC(theCase, monitor, motherboard);
+        thePC.getMonitor().drawPixelAt(1500, 1200, "red");
+        thePC.getMotherboard().loadProgram("Windows 1.0");
+        thePC.getTheCase().pressPowerButton();
+
     }
+
 }
---
+++ Section7/Composition/src/com/timbuchalka/Vehicle.java
@@ -0,0 +1,9 @@
+package com.timbuchalka;
+
+public class Vehicle {
+    private String name;
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+}
