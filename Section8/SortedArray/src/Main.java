e2110d6f7007d9e9d88079ab0c890439c5ce466f
Vladislavs K <vladislavs.kcs@gmail.com>
2022-08-05 02:36:02 +0300

    S8: Coding Exercise 41: Sorted Array

--- Section8/SortedArray/src/Main.java
+++ Section8/SortedArray/src/Main.java
@@ -1,2 +1,24 @@
 public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Enter you 'int' numbers: ");
+        int[] inputArray = SortedArray.getIntegers(5);
+
+        System.out.println("Unsorted array: ");
+        SortedArray.printArray(inputArray);
+
+        System.out.println();
+
+        System.out.println("----------Sorting----------");
+        int[] sortedArray = SortedArray.sortIntegers(inputArray);
+
+        System.out.println("Unsorted array: ");
+        SortedArray.printArray(inputArray);
+
+        System.out.println();
+
+        System.out.println("Descending sorted array: ");
+        SortedArray.printArray(sortedArray);
+
+    }
 }
--- Section8/SortedArray/src/SortedArray.java
+++ Section8/SortedArray/src/SortedArray.java
@@ -1,2 +1,40 @@
+import java.util.Scanner;
+
 public class SortedArray {
+
+    public static int[] getIntegers(int arraySize){
+
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[arraySize];
+
+        for (int i = 0; i < array.length; i++){
+            array[i] = scanner.nextInt();
+        }
+
+        return array;
+    }
+
+    public static void printArray(int[] array){
+        for (int i = 0; i < array.length; i++){
+            System.out.println("Element " + i + " contents " + array[i]);
+        }
+    }
+
+    public static int[] sortIntegers(int[] array){
+
+        int[] sortedArray = array.clone();
+
+        for (int i = 0; i < sortedArray.length - 1 ; i++){
+            for (int j = 0; j < sortedArray.length - 1 - i; j++){
+                if (sortedArray[j] < sortedArray[j + 1]){
+                    int temp = sortedArray[j + 1];
+                    sortedArray[j + 1] = sortedArray[j];
+                    sortedArray[j] = temp;
+                }
+            }
+        }
+
+        return sortedArray;
+    }
+
 }
