54850ed599400c42aa8620967afbe5b8cec6f95d
Vladislavs K <vladislavs.kcs@gmail.com>
2022-08-06 02:48:50 +0300

    S8: Coding Exercise 44: Mobile Phone (ArrayList Challenge)

--- Section8/MobilePhone/src/Main.java
+++ Section8/MobilePhone/src/Main.java
@@ -1,2 +1,30 @@
 public class Main {
+    public static void main(String[] args) {
+        MobilePhone mp = new MobilePhone("12345678");
+
+        Contact Bob = new Contact("Bob", "31415926");
+        Contact Alice = new Contact("Alice", "16180339");
+        Contact Tom = new Contact("Tom", "11235813");
+        Contact Jane = new Contact("Jane", "23571113");
+        Contact Bob2 = new Contact("Bob", "00000000");
+        Contact Alice2 = new Contact("Alice", "22222222");
+
+        mp.addNewContact(Bob);
+        mp.addNewContact(Alice);
+        mp.addNewContact(Tom);
+        mp.addNewContact(Jane);
+        mp.addNewContact(Bob2); //Should not be added to the contact list
+
+        mp.printContacts();
+
+        mp.updateContact(Alice, Alice2);
+        mp.printContacts();
+
+        mp.removeContact(Bob2);
+        mp.printContacts();
+
+        mp.removeContact(Tom);
+        mp.printContacts();
+
+    }
 }
--- Section8/MobilePhone/src/MobilePhone.java
+++ Section8/MobilePhone/src/MobilePhone.java
@@ -1,2 +1,73 @@
+import java.util.ArrayList;
+
 public class MobilePhone {
+    private String myNumber;
+    private ArrayList<Contact> myContacts;
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<>();
+    }
+
+    public boolean addNewContact(Contact contact) {
+        int isInContactList = findContact(contact.getName());
+
+        if (isInContactList < 0) {
+            myContacts.add(contact);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean updateContact(Contact oldContact, Contact newContact) {
+        int contactIndex = findContact(oldContact);
+
+        if (contactIndex >= 0) {
+            myContacts.set(contactIndex, newContact);
+            return true;//Returns true if the contact exists and was updated successfully,
+        }
+
+        return false;  //or returns false if the contact doesn't exists.
+    }
+
+    public boolean removeContact(Contact contact) {
+        int contactIndex = findContact(contact);
+
+        if (contactIndex >= 0) {
+            myContacts.remove(contactIndex);
+            return true;//Returns true if the contact exists and was removed successfully,
+        }
+
+        return false; //or returns false if the contact doesn't exists.
+    }
+
+    private int findContact(Contact contact) {
+        return myContacts.indexOf(contact);
+    }
+
+    private int findContact(String contactName) {
+        return myContacts.indexOf(queryContact(contactName));
+    }
+
+    public Contact queryContact(String contactName) {
+        for (int i = 0; i < myContacts.size(); i++){
+            if (myContacts.get(i).getName().equals(contactName)){
+                return myContacts.get(i);
+            }
+        }
+
+        return null;
+    }
+
+    public void printContacts() {
+        System.out.println("Contact List:");
+        for (int i = 0; i < myContacts.size(); i++) {
+            String name = myContacts.get(i).getName();
+            String pNumber = myContacts.get(i).getPhoneNumber();
+
+            System.out.println((i + 1) + ". " + name + " -> " + pNumber);
+        }
+    }
+
 }
