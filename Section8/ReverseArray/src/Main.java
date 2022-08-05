4e2ae1da26e07f0622be8881c34c254ae9f385f5
Vladislavs K <vladislavs.kcs@gmail.com>
2022-08-05 04:47:31 +0300

    S8: Coding Exercise 43: Reverse Array

--- Section8/ReverseArray/src/Main.java
+++ Section8/ReverseArray/src/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+    public static void main(String[] args) {
+        int[] myArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        ReverseArray.rv(myArray);
+    }
 }
--- Section8/ReverseArray/src/ReverseArray.java
+++ Section8/ReverseArray/src/ReverseArray.java
@@ -1,2 +1,21 @@
+import java.util.Arrays;
+
 public class ReverseArray {
+
+    private static void reverse(int[] array){
+        int[] reverseArray = new int[array.length];
+
+        System.out.println("Array = " + Arrays.toString(array));
+
+        for (int i = 0; i < reverseArray.length; i++){
+            reverseArray[i] = array[array.length - 1 - i];
+        }
+
+        System.out.println("Reversed array = " + Arrays.toString(reverseArray));
+
+    }
+
+    public static void rv(int[] array){
+        reverse(array);
+    }
 }
