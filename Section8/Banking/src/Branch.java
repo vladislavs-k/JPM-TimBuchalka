77a04917267133db275592b9886289316b90da87
Vladislavs K <vladislavs.kcs@gmail.com>
2022-08-06 23:20:56 +0300

    Add functionality of the Branch.java & Customer.java

--- Section8/Banking/src/Branch.java
+++ Section8/Banking/src/Branch.java
@@ -1,2 +1,51 @@
+import java.util.ArrayList;
+
 public class Branch {
+    private String name;
+    private ArrayList<Customer> customers;
+
+    public Branch(String name) {
+        this.name = name;
+        this.customers = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    public boolean newCustomer(String customerName, double initTransaction){
+        Customer customer = findCustomer(customerName);
+
+        if (customer == null){
+            customers.add(new Customer(customerName, initTransaction));
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean addCustomerTransaction(String customerName, double transaction){
+        Customer customer = findCustomer(customerName);
+
+        if  (customer != null){
+            customer.addTransaction(transaction);
+            return true;
+        }
+        return false;
+    }
+
+    private Customer findCustomer(String customerName){
+        for (Customer theCustomer : customers){
+            if (theCustomer.getName().equals(customerName)){
+                return theCustomer;
+            }
+        }
+
+        return null;
+    }
+
 }
--- Section8/Banking/src/Customer.java
+++ Section8/Banking/src/Customer.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+
 public class Customer {
+    private String name;
+    private ArrayList<Double> transactions;
+
+    public Customer(String name, double initTransaction){
+        this.name = name;
+        this.transactions = new ArrayList<>();
+        addTransaction(initTransaction);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Double> getTransactions() {
+        return transactions;
+    }
+
+    public void addTransaction(double transaction){
+        this.transactions.add(transaction);
+    }
 }
