b56cc1bffe627589885a3e5de44b5bd9582e9660
Vladislavs K <vladislavs.kcs@gmail.com>
2022-08-12 23:34:57 +0300

    S10: Our Generics Class (Part 2)

--- Section9/OurGenericsClass/src/com/timbuchalka/Team.java
+++ Section9/OurGenericsClass/src/com/timbuchalka/Team.java
@@ -2,14 +2,14 @@ package com.timbuchalka;
 
 import java.util.ArrayList;
 
-public class Team {
+public class Team<T extends Player> {
     private String name;
     private int played = 0;
     private int won = 0;
     private int lost = 0;
     private int tied = 0;
 
-    private ArrayList<Player> members = new ArrayList<>();
+    private ArrayList<T> members = new ArrayList<>();
 
     public Team(String name) {
         this.name = name;
@@ -19,7 +19,7 @@ public class Team {
         return name;
     }
 
-    public boolean addPlayer(Player player){
+    public boolean addPlayer(T player){
         if (members.contains(player)){
             System.out.println(player.getName() + " is already on team");
             return false;
@@ -34,18 +34,25 @@ public class Team {
         return this.members.size();
     }
 
-    public void matchResult(Team opponent, int ourScore, int theirScore){
+    public void matchResult(Team<T> opponent, int ourScore, int theirScore){
+
+        String message;
+
         if(ourScore > theirScore) {
             won++;
+            message = " beat ";
         }else if (ourScore == theirScore){
             tied++;
+            message = " drew with ";
         } else {
             lost++;
+            message = " lost to ";
         }
 
         played++;
 
         if (opponent != null){
+            System.out.println(this.getName() + message + opponent.getName());
             opponent.matchResult(null, theirScore, ourScore);
         }
 
