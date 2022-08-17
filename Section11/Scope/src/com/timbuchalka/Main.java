4bc398d649fca03904ae230ac424be3d17e24287
Vladislavs K <vladislavs.kcs@gmail.com>
2022-08-17 05:36:39 +0300

    S11: Scope Part 2 and Visibility

--- Section11/Scope/src/com/timbuchalka/Main.java
+++ Section11/Scope/src/com/timbuchalka/Main.java
@@ -3,15 +3,20 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-	    String privateVar = "this is private to main()";
+	    String varFour = "this is private to main()";
 
         ScopeCheck scopeInstance = new ScopeCheck();
-        System.out.println("scopeInstance privateVar is " + scopeInstance.getPrivateVar());
-        System.out.println(privateVar);
+        scopeInstance.useInner();
 
-        scopeInstance.timesTwo();
-        System.out.println("************************************************");
-        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
-        innerClass.timesTwo();
+//        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+//        System.out.println("varThree is not accessible here " + innerClass.varThree);
+
+//        System.out.println("scopeInstance varOne is " + scopeInstance.getVarOne());
+//        System.out.println(varFour);
+//
+//        scopeInstance.timesTwo();
+//        System.out.println("************************************************");
+//        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+//        innerClass.timesTwo();
     }
 }
