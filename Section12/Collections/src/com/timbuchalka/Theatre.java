f13b388ead585bc241ea20a1a8e9ce43c450470a
Vladislavs K <vladislavs.kcs@gmail.com>
2022-08-18 03:07:04 +0300

    S12: Collections List Methods

--- Section12/Collections/src/com/timbuchalka/Theatre.java
+++ Section12/Collections/src/com/timbuchalka/Theatre.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Theatre {
     private final String theatreName;
-    private List<Seat> seats = new ArrayList<>();
+    public List<Seat> seats = new ArrayList<>();
 
     public Theatre(String theatreName, int numRows, int seatsPerRow) {
         this.theatreName = theatreName;
@@ -75,7 +75,7 @@ public class Theatre {
         }
     }
 
-    private class Seat implements Comparable<Seat>{
+    public class Seat implements Comparable<Seat>{
         private final String seatNumber;
         private boolean reserved = false;
 
