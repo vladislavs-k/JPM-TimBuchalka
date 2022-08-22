d943d2f5eb76eec22d8cfac9d42afb8575d2788c
Vladislavs K <vladislavs.kcs@gmail.com>
2022-08-23 02:48:36 +0300

    S12: Finishing Off Sets

--- Section12/SetAndHashSet/src/com/timbuchalka/SetMain.java
+++ Section12/SetAndHashSet/src/com/timbuchalka/SetMain.java
@@ -43,5 +43,43 @@ public class SetMain {
         String[] divineWords = {"to", "err", "is", "human", "to", "forgive", "divine"};
         divine.addAll(Arrays.asList(divineWords));
 
+        System.out.println("nature - divine:");
+        Set<String> diff1 = new HashSet<>(nature);
+        diff1.removeAll(divine);
+        printSet(diff1);
+
+        System.out.println("divine - nature");
+        Set<String> diff2 = new HashSet<>(divine);
+        diff2.removeAll(nature);
+        printSet(diff2);
+
+        Set<String> unionTest = new HashSet<>(nature);
+        unionTest.addAll(divine);
+        Set<String> intersectionTest = new HashSet<>(nature);
+        intersectionTest.retainAll(divine);
+
+        System.out.println("Symmetric difference");
+        unionTest.removeAll(intersectionTest);
+        printSet(unionTest);
+
+        if(nature.containsAll(divine)) {
+            System.out.println("divine is a subset of nature");
+        }
+
+        if(nature.containsAll(intersectionTest)) {
+            System.out.println("intersection is  subset of nature");
+        }
+        
+        if(divine.containsAll(intersectionTest)) {
+            System.out.println("intersection is a subset of divine");
+        }
+    }
+
+    private static void printSet(Set<String> set){
+        System.out.print("\t");
+        for (String s : set){
+            System.out.print(s + " ");
+        }
+        System.out.println();
     }
 }
