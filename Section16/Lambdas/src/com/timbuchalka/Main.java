84f406fcd3b7cea9b2c3c29196a1057cc54b9a60
Vladislavs K <vladislavs.kcs@gmail.com>
2022-08-25 01:11:01 +0300

    S16: Lambda Expressions Continued

--- Section16/Lambdas/src/com/timbuchalka/Main.java
+++ Section16/Lambdas/src/com/timbuchalka/Main.java
@@ -50,8 +50,26 @@ public class Main {
             System.out.println(employee.getName());
         }
 
+//        String sillyString = doStringStuff(new UpperConcat() {
+//            @Override
+//            public String upperAndConcat(String s1, String s2) {
+//                return s1.toUpperCase() + " " + s2.toUpperCase();
+//            }
+//        },
+//                employees.get(0).getName(), employees.get(1).getName());
+//        System.out.println(sillyString);
+
+        UpperConcat us = ( (s1, s2) -> s1.toUpperCase() + " " + s2.toUpperCase() );
+        String sillyString = doStringStuff(us, employees.get(0).getName(), employees.get(1).getName());
+        System.out.println(sillyString);
+
+    }//END main()
+
+    public final static String doStringStuff(UpperConcat uc, String s1, String s2){
+        return uc.upperAndConcat(s1, s2);
     }
-}
+
+}//END class Main
 
 class Employee{
     private String name;
@@ -77,6 +95,10 @@ class Employee{
     public void setAge(int age) {
         this.age = age;
     }
+}
+
+interface UpperConcat{
+    String upperAndConcat(String s1, String s2);
 }
 
 //class CodeToRun implements Runnable{
