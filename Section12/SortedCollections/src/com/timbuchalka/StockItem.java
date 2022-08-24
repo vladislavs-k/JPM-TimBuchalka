3b087e5b05b5cbdedcf3e388a016da3fde357c30
Vladislavs K <vladislavs.kcs@gmail.com>
2022-08-24 22:44:56 +0300

    S12: Challenge (Part 1 to 4)

--- Section12/SortedCollections/src/com/timbuchalka/StockItem.java
+++ Section12/SortedCollections/src/com/timbuchalka/StockItem.java
@@ -4,6 +4,7 @@ public class StockItem implements Comparable<StockItem> {
     private final String name;
     private double price;
     private int quantityStock = 0;
+    private int reserved = 0;
 
     public StockItem(String name, double price) {
         this.name = name;
@@ -25,8 +26,8 @@ public class StockItem implements Comparable<StockItem> {
         return price;
     }
 
-    public int quantityInStock() {
-        return quantityStock;
+    public int availableQuantity() {
+        return quantityStock - reserved;
     }
 
     public void setPrice(double price) {
@@ -40,6 +41,31 @@ public class StockItem implements Comparable<StockItem> {
         if(newQuantity >= 0){
             this.quantityStock = newQuantity;
         }
+    }
+
+    public int reserveStock(int quantity){
+        if(quantity <= availableQuantity() ){     // if( quantity <= (quantityStock - reserved) )
+            reserved += quantity;
+            return quantity;
+        }
+        return 0;
+    }
+
+    public int unreserveStock(int quantity){
+        if(quantity <= reserved){
+            reserved -= quantity;
+            return quantity;
+        }
+        return 0;
+    }
+
+    public int finaliseStock(int quantity){
+        if(quantity <= reserved){
+            quantityStock -= quantity;
+            reserved -= quantity;
+            return quantity;
+        }
+        return 0;
     }
 
     @Override
@@ -64,7 +90,7 @@ public class StockItem implements Comparable<StockItem> {
 
     @Override
     public int compareTo(StockItem o) {
-        System.out.println("Entering StockItem.compareTo()");
+//        System.out.println("Entering StockItem.compareTo()");
         if(this == o){
             return 0;
         }
@@ -78,6 +104,6 @@ public class StockItem implements Comparable<StockItem> {
 
     @Override
     public String toString() {
-        return this.name + " : price " + this.price;
+        return this.name + " : price " + this.price + ". Reserved: " + this.reserved;
     }
 }
--- Section12/SortedCollections/src/com/timbuchalka/StockList.java
+++ Section12/SortedCollections/src/com/timbuchalka/StockList.java
@@ -1,7 +1,6 @@
 package com.timbuchalka;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -16,22 +15,47 @@ public class StockList {
         if(item != null){
             StockItem inStock = list.getOrDefault(item.getName(), item);
             if(inStock != item){
-                item.adjustStock(inStock.quantityInStock());
+                item.adjustStock(inStock.availableQuantity());
             }
             list.put(item.getName(), item);
-            return item.quantityInStock();
+            return item.availableQuantity();
+        }
+        return 0;
+    }
+
+    public int reserveStock(String item, int quantity){
+        StockItem inStock = list.get(item);
+
+        if( (inStock != null) && (quantity > 0) ){
+            return inStock.reserveStock(quantity);
+        }
+        return 0;
+    }
+
+    public int unreserveStock(String item, int quantity){
+        StockItem inStock = list.get(item);
+
+        if( (inStock != null) && (quantity > 0) ){
+            return inStock.unreserveStock(quantity);
         }
         return 0;
     }
 
     public int sellStock(String item, int quantity){
-        StockItem inStock = list.getOrDefault(item, null);
+        StockItem inStock = list.get(item);
 
-        if( (inStock != null) && (inStock.quantityInStock() >= quantity) && (quantity > 0) ){
-            inStock.adjustStock(-quantity);
-            return quantity;
+        if( (inStock != null) && (quantity > 0) ){
+            return inStock.finaliseStock(quantity);
         }
         return 0;
+
+//        StockItem inStock = list.getOrDefault(item, null);
+//
+//        if( (inStock != null) && (inStock.availableQuantity() >= quantity) && (quantity > 0) ){
+//            inStock.adjustStock(-quantity);
+//            return quantity;
+//        }
+//        return 0;
     }
 
     public StockItem get(String key){
@@ -57,9 +81,9 @@ public class StockList {
         for (Map.Entry<String, StockItem> item : list.entrySet()){
             StockItem stockItem = item.getValue();
 
-            double itemValue = stockItem.getPrice() * stockItem.quantityInStock();
+            double itemValue = stockItem.getPrice() * stockItem.availableQuantity();
 
-            s = s + stockItem + ". There are " + stockItem.quantityInStock() + " in stock. Value of items: ";
+            s = s + stockItem + ". There are " + stockItem.availableQuantity() + " in stock. Value of items: ";
             s = s + String.format("%.2f", itemValue) + "\n";
             totalCost += itemValue;
         }
