0e32aac3f42994af731fbf7c7865721c1c5ad323
Vladislavs K <vladislavs.kcs@gmail.com>
2022-08-25 05:59:31 +0300

    S16: More on Predicates & Suppliers

--- Section16/Employees/src/com/timbuchalka/Main.java
+++ Section16/Employees/src/com/timbuchalka/Main.java
@@ -2,7 +2,10 @@ package com.timbuchalka;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.function.IntPredicate;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Main {
 
@@ -24,6 +27,31 @@ public class Main {
 
         printEmployeesByAge(employees, "Employees over 30", employee -> employee.getAge() > 30);
         printEmployeesByAge(employees, "Employees 30 and under", employee -> employee.getAge() <= 30);
+        printEmployeesByAge(employees, "\nEmployees youner than 25", new Predicate<Employee>() {
+            @Override
+            public boolean test(Employee employee) {
+                return employee.getAge() < 25;
+            }
+        });
+
+        IntPredicate greaterThan15 = i -> i > 15;
+        IntPredicate lessThan100 = i -> i < 100;
+
+        System.out.println(greaterThan15.test(10));
+        int a = 20;
+        System.out.println(greaterThan15.test(a + 5));
+
+        System.out.println(greaterThan15.and(lessThan100).test(50));
+        System.out.println(greaterThan15.and(lessThan100).test(15));
+
+        Random random = new Random();
+        Supplier<Integer> randomSupplier = () -> random.nextInt(1000);
+
+        for (int i = 0; i < 10; i++){
+            System.out.println(randomSupplier.get());
+        }
+
+
     }
 
     private static void printEmployeesByAge(List<Employee> employees,
