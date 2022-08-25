3cf3a2365eb22217dedf26db65f40e22b738bf7f
Vladislavs K <vladislavs.kcs@gmail.com>
2022-08-25 04:25:39 +0300

    S16: Create project 'Employees'

--- Section16/Lambdas/src/com/timbuchalka/Main.java
+++ Section16/Lambdas/src/com/timbuchalka/Main.java
@@ -41,7 +41,7 @@ public class Main {
             System.out.println(employee.getName());
             System.out.println(employee.getAge());
         });
-        
+
 //        for(Employee employee : employees){
 //            System.out.println(employee.getName());
 //            System.out.println(employee.getAge());
