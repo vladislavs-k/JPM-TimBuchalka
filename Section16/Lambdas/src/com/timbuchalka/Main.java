13c112322212ee5b4ea547cc795cd30819d4fd9e
Vladislavs K <vladislavs.kcs@gmail.com>
2022-08-25 04:18:15 +0300

    S16: Scope and Functional Programming

--- Section16/Lambdas/src/com/timbuchalka/Main.java
+++ Section16/Lambdas/src/com/timbuchalka/Main.java
@@ -36,6 +36,26 @@ public class Main {
         employees.add(jack);
         employees.add(snow);
 
+
+        employees.forEach(employee -> {
+            System.out.println(employee.getName());
+            System.out.println(employee.getAge());
+        });
+        
+//        for(Employee employee : employees){
+//            System.out.println(employee.getName());
+//            System.out.println(employee.getAge());
+////            new Thread( () -> System.out.println(employee.getAge())).start();
+//        }
+
+//        System.out.println("**********************************************8");
+//        for (int i = 0; i < employees.size(); i++){
+//            Employee employee = employees.get(i);
+//            System.out.println(employee.getName());
+//            new Thread( () -> System.out.println(employee.getAge())).start();
+//        }
+
+
 ////        Collections.sort(employees, new Comparator<Employee>() {
 ////            @Override
 ////            public int compare(Employee employee1, Employee employee2) {
@@ -66,9 +86,9 @@ public class Main {
 //        String sillyString = doStringStuff(us, employees.get(0).getName(), employees.get(1).getName());
 //        System.out.println(sillyString);
 
-        AnotherClass anotherClass = new AnotherClass();
-        String s = anotherClass.doSomething();
-        System.out.println(s);
+//        AnotherClass anotherClass = new AnotherClass();
+//        String s = anotherClass.doSomething();
+//        System.out.println(s);
 
     }//END main()
 
@@ -120,9 +140,11 @@ class AnotherClass{
 
         UpperConcat uc = (s1, s2) -> {
             System.out.println("The lambda expression's class is " + getClass().getSimpleName());
+            System.out.println("i in the lambda expression = " + i);
             String result = s1.toUpperCase() + " " + s2.toUpperCase();
             return result;
         };
+
 
 //        UpperConcat uc = new UpperConcat() {
 //            @Override
@@ -147,6 +169,21 @@ class AnotherClass{
 //            }
 //        },
 //                "String1", "String2");
+    }
+
+    public void printValue(){
+        int number = 25;
+
+        Runnable r = () -> {
+            try{
+                Thread.sleep(5000);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            System.out.println("The value is " + number);
+        };
+
+        new Thread(r).start();
     }
 }
 
