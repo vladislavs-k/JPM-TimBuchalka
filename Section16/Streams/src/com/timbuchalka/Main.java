6dcbbfba9dd111477d971c33d849dda3a7aed3c8
Vladislavs K <vladislavs.kcs@gmail.com>
2022-08-29 06:25:28 +0300

    S16: Streams

--- Section16/Streams/src/com/timbuchalka/Main.java
+++ Section16/Streams/src/com/timbuchalka/Main.java
@@ -1,8 +1,38 @@
 package com.timbuchalka;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<String> someBingoNumber = Arrays.asList(
+                "N40", "N36",
+                "B12", "B6",
+                "G53", "G49", "G60", "G50", "g64",
+                "I26", "I17", "I29",
+                "O71");
+
+        List<String> gNumbers = new ArrayList<>();
+
+//        someBingoNumber.forEach(number -> {
+//            if(number.toUpperCase().startsWith("G")){
+//                gNumbers.add(number);
+////                System.out.println(number);
+//            }
+//        });
+//
+//        gNumbers.sort( (s1, s2) -> s1.compareTo(s2));
+//        gNumbers.forEach(s -> System.out.println(s));
+
+        someBingoNumber
+                .stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.startsWith("G"))
+                .sorted()
+                .forEach(System.out::println);
+
     }
 }
