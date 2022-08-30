2cc5a81ba9113dd1c9065551e76c80c53d7173f8
Vladislavs K <vladislavs.kcs@gmail.com>
2022-08-31 01:04:46 +0300

    S17: Quantifiers and the Pattern and Matcher classes

--- Section17/RegularExpressions/src/com/timbuchalka/Main.java
+++ Section17/RegularExpressions/src/com/timbuchalka/Main.java
@@ -1,5 +1,8 @@
 package com.timbuchalka;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -54,6 +57,29 @@ public class Main {
         System.out.println(hasWhitespace.replaceAll("\\w", "X"));
 
         System.out.println(hasWhitespace.replaceAll("\\b", "X"));
+
+
+        String thirdAlphanumeric = "abcDeeeF12Ghhiiiijkl99z";
+        System.out.println(thirdAlphanumeric.replaceAll("^abcDe{3}", "YYY"));
+        System.out.println(thirdAlphanumeric.replaceAll("^abcDe+", "YYY"));
+        System.out.println(thirdAlphanumeric.replaceAll("^abcDe*", "YYY"));
+        System.out.println(thirdAlphanumeric.replaceAll("^abcDe{2,5}", "YYY"));
+        System.out.println(thirdAlphanumeric.replaceAll("h+i*j", "Y"));
+
+
+
+        StringBuilder htmlText = new StringBuilder("<h1>My Heading</h1>");
+        htmlText.append("<h2>Sub-heading</h2>");
+        htmlText.append("<p>This is a paragraph about something.</p>");
+        htmlText.append("<p>This is another paragraph about something else.</p>");
+        htmlText.append("<h2>Summary</h2>");
+        htmlText.append("<p>Here is the summary.</p>");
+
+        String h2Pattern = ".*<h2>.*";
+        Pattern pattern = Pattern.compile(h2Pattern);
+        Matcher matcher = pattern.matcher(htmlText);
+        System.out.println(matcher.matches());
+
 
 
 
