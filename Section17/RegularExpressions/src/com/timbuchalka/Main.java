122201b935c33b9136db7e376cef10779c37585d
Vladislavs K <vladislavs.kcs@gmail.com>
2022-08-31 00:42:38 +0300

    S17: Character classes and Boundary Matchers

--- Section17/RegularExpressions/src/com/timbuchalka/Main.java
+++ Section17/RegularExpressions/src/com/timbuchalka/Main.java
@@ -25,5 +25,37 @@ public class Main {
         System.out.println(alphanumeric.replaceAll("[aei]", "I replaced a letter here"));
         System.out.println(alphanumeric.replaceAll("[aei][Fj]", "X"));
 
+        System.out.println("harry".replaceAll("[Hh]arry", "Harry"));
+
+
+
+        String newAlphanumeric = "abcDeeeF12Ghhiiiijkl99z";
+
+        System.out.println(newAlphanumeric.replaceAll("[^ej]", "X"));
+
+        System.out.println(newAlphanumeric.replaceAll("[abcdef345678]", "X"));
+        System.out.println(newAlphanumeric.replaceAll("[a-f3-8]", "X"));
+
+        System.out.println(newAlphanumeric.replaceAll("[a-fA-F3-8]", "X"));
+        System.out.println(newAlphanumeric.replaceAll("(?i)[a-f3-8]", "X"));
+
+        System.out.println(newAlphanumeric.replaceAll("[0-9]", "X"));
+        System.out.println(newAlphanumeric.replaceAll("\\d", "X"));
+        System.out.println(newAlphanumeric.replaceAll("\\D", "X"));
+
+
+        String hasWhitespace = "I have blanks and\ta tab, and also a newline\n";
+        System.out.println(hasWhitespace);
+        System.out.println(hasWhitespace.replaceAll("\\s", ""));
+        System.out.println(hasWhitespace.replaceAll("\t", "X"));
+        System.out.println("###" + hasWhitespace.replaceAll("\\S", "") + "###");
+
+        System.out.println(newAlphanumeric.replaceAll("\\w", "X"));
+        System.out.println(hasWhitespace.replaceAll("\\w", "X"));
+
+        System.out.println(hasWhitespace.replaceAll("\\b", "X"));
+
+
+
     }
 }
