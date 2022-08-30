fcc0c9d079cb465fc514ed306b4e73b2944db09c
Vladislavs K <vladislavs.kcs@gmail.com>
2022-08-31 01:37:54 +0300

    S17: Matcher find and Group Methods

--- Section17/RegularExpressions/src/com/timbuchalka/Main.java
+++ Section17/RegularExpressions/src/com/timbuchalka/Main.java
@@ -75,10 +75,38 @@ public class Main {
         htmlText.append("<h2>Summary</h2>");
         htmlText.append("<p>Here is the summary.</p>");
 
-        String h2Pattern = ".*<h2>.*";
+        String h2Pattern = "<h2>";
         Pattern pattern = Pattern.compile(h2Pattern);
         Matcher matcher = pattern.matcher(htmlText);
         System.out.println(matcher.matches());
+
+        matcher.reset();
+        int count = 0;
+        while (matcher.find()){
+            count++;
+            System.out.println("Occurrence " + count + " : " + matcher.start() + " to " + matcher.end());
+        }
+
+
+
+        String h2GroupPattern = "(<h2>.*?</h2>)";
+        Pattern groupPattern = Pattern.compile(h2GroupPattern);
+        Matcher groupMatcher = groupPattern.matcher(htmlText);
+        System.out.println(groupMatcher.matches());
+        groupMatcher.reset();
+
+        while (groupMatcher.find()){
+            System.out.println("Occurrence " + groupMatcher.group(1));
+        }
+
+        String h2TextGroups = "(<h2>)(.+?)(</h2>)";
+        Pattern h2TextPattern = Pattern.compile(h2TextGroups);
+        Matcher h2TextMatcher = h2TextPattern.matcher(htmlText);
+
+        while (h2TextMatcher.find()){
+            System.out.println("Occurrence " + h2TextMatcher.group(2));
+        }
+
 
 
 
