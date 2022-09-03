36dc270f405bef3b4ad32014d423a799e8a88137
Vladislavs K <vladislavs.kcs@gmail.com>
2022-09-03 23:51:47 +0300

    S18: Introduction to Debugging

--- Section18/DebuggerDemo/src/com/timbuchalka/Main.java
+++ Section18/DebuggerDemo/src/com/timbuchalka/Main.java
@@ -3,6 +3,11 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        StringUtilities utils = new StringUtilities();
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < 10) {
+            utils.addChar(sb, 'a');
+        }
+        System.out.println(sb);
     }
 }
