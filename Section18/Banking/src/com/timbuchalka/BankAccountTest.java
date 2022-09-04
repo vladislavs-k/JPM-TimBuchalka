fcfb02f02398c7dce38b03454dc97b7f563ad681
Vladislavs K <vladislavs.kcs@gmail.com>
2022-09-04 04:23:31 +0300

    S18: Introduction to Unit Testing with JUnit

---
+++ Section18/Banking/src/com/timbuchalka/BankAccountTest.java
@@ -0,0 +1,23 @@
+package com.timbuchalka;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BankAccountTest {
+
+    @Test
+    public void deposit() {
+        fail("This test has yet to be implemented");
+    }
+
+    @Test
+    public void withdraw() {
+        fail("This test has yet to be implemented");
+    }
+
+    @Test
+    public void getBalance() {
+        fail("This test has yet to be implemented");
+    }
+}
