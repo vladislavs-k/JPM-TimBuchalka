b1e988496b09dbeeac7af4243881e0c044b9b42d
Vladislavs K <vladislavs.kcs@gmail.com>
2022-09-05 01:21:23 +0300

    S19: More with SQL

--- Section19/SQLqueries/JPM-TimBuc.sql
+++ Section19/SQLqueries/JPM-TimBuc.sql
@@ -1,3 +1,5 @@
+SET SQL_SAFE_UPDATES = 0;
+
 CREATE DATABASE jpm_timbuc;
 
 USE jpm_timbuc;
@@ -23,3 +25,20 @@ INSERT INTO contacts VALUES ("Brian", 1234, "brian@email.com");
 INSERT INTO contacts VALUES ("Steve", 87654);
 
 INSERT INTO contacts (name, phone) VALUES ("Steve", 87654);
+
+INSERT INTO contacts VALUES ("Avril", "+61 (0)87654321", "avril@email.com.au"); -- In SQLite it'll work but not int MySQL.
+
+UPDATE contacts SET email="steve@email.com";
+
+UPDATE contacts SET email="steve@email.com" WHERE name = "Steve";
+
+UPDATE contacts SET email="steve@hisemail.com" WHERE name = 'Steve';
+
+SELECT * FROM contacts WHERE name = "Brian";
+
+SELECT phone, email FROM contacts WHERE name = "Brian";
+
+DELETE FROM contacts WHERE phone=1234;
+
+SHOW TABLES;
+
