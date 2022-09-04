777a6f371d46caa09e555f1785c013b02d6f84c4
Vladislavs K <vladislavs.kcs@gmail.com>
2022-09-04 04:48:06 +0300

    S18: Asserts in Junit

--- Section18/Banking/src/com/timbuchalka/BankAccountTest.java
+++ Section18/Banking/src/com/timbuchalka/BankAccountTest.java
@@ -8,7 +8,9 @@ public class BankAccountTest {
 
     @Test
     public void deposit() {
-        fail("This test has yet to be implemented");
+        BankAccount bankAccount = new BankAccount("Vlad", "Kcs", 1000.0, BankAccount.CHECKING);
+        double balance = bankAccount.deposit(200.0, true);
+        assertEquals(1200.0, balance, 0);
     }
 
     @Test
@@ -17,7 +19,25 @@ public class BankAccountTest {
     }
 
     @Test
-    public void getBalance() {
-        fail("This test has yet to be implemented");
+    public void getBalance_deposit() {
+        BankAccount bankAccount = new BankAccount("Vlad", "Kcs", 1000.0, BankAccount.CHECKING);
+        bankAccount.deposit(200.0, true);
+        assertEquals(1200.0, bankAccount.getBalance(), 0);
     }
+
+    @Test
+    public void getBalance_withdraw() {
+        BankAccount bankAccount = new BankAccount("Vlad", "Kcs", 1000.0, BankAccount.CHECKING);
+        bankAccount.withdraw(200.0, true);
+        assertEquals(800.0, bankAccount.getBalance(), 0);
+    }
+
+    @Test
+    public void isChecking_true(){
+        BankAccount bankAccount = new BankAccount("Vlad", "Kcs", 1000.0, BankAccount.CHECKING);
+//        assertEquals(true, bankAccount.isChecking());
+        assertTrue("The account is NOT a checking account", bankAccount.isChecking());
+    }
+
+
 }
