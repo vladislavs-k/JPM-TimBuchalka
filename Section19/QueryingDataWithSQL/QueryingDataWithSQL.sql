f31becc306f9f4f629b6fee5ed7032d09846004e
Vladislavs K <vladislavs.kcs@gmail.com>
2022-09-05 03:39:32 +0300

    S19: More Complex Joins

--- Section19/QueryingDataWithSQL/QueryingDataWithSQL.sql
+++ Section19/QueryingDataWithSQL/QueryingDataWithSQL.sql
@@ -34,3 +34,28 @@ SELECT songs.track, songs.title, albums.name FROM songs INNER JOIN albums ON son
 SELECT songs.track, songs.title, albums.name 
 FROM songs INNER JOIN albums ON songs.album = albums._id
 ORDER BY albums.name, songs.track;
+
+SELECT albums.name, songs.track, songs.title
+FROM songs INNER JOIN albums ON songs.album = albums._id
+ORDER BY albums.name, songs.track;
+
+SELECT artists.name, albums.name
+FROM albums INNER JOIN artists ON albums.artist = artists._id
+ORDER BY artists.name;
+
+SELECT artists.name, albums.name, songs.track, songs.title FROM songs 
+INNER JOIN albums ON songs.album = albums._id
+INNER JOIN artists ON albums.artist = artists._id
+ORDER BY artists.name, albums.name, songs.track;
+
+SELECT artists.name, albums.name, songs.track, songs.title FROM songs 
+INNER JOIN albums ON songs.album = albums._id
+INNER JOIN artists ON albums.artist = artists._id
+WHERE albums._id = 19
+ORDER BY artists.name, albums.name, songs.track;
+
+SELECT artists.name, albums.name, songs.track, songs.title FROM songs 
+INNER JOIN albums ON songs.album = albums._id
+INNER JOIN artists ON albums.artist = artists._id
+WHERE albums.name = "Doolittle"
+ORDER BY artists.name, albums.name, songs.track;
