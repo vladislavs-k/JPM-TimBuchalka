803703d00c3d027cf5e5cbcfb0997755585b82c6
Vladislavs K <vladislavs.kcs@gmail.com>
2022-09-06 02:23:31 +0300

    S19: The Music SQLite Database

---
+++ Section19/Music/SQLqueries/MusicQueries.sql
@@ -0,0 +1,5 @@
+USE music;
+
+SELECT * FROM artists;
+SELECT * FROM albums;
+SELECT * FROM songs;
--- Section19/Music/src/com/timbuchalka/Main.java
+++ Section19/Music/src/com/timbuchalka/Main.java
@@ -1,8 +1,20 @@
 package com.timbuchalka;
 
+import com.timbuchalka.model.DataSource;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        DataSource dataSource = new DataSource();
+
+        if(!dataSource.open()){
+            System.out.println("Can't open data source");
+            return;
+        }
+
+
+
+        dataSource.close();
     }
 }
