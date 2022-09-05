082e387f551d97ef234e9d357e48d0f1ab7b8f66
Vladislavs K <vladislavs.kcs@gmail.com>
2022-09-05 03:16:24 +0300

    S19: SQL Order by and Joins

--- Section19/QueryingDataWithSQL/QueryingDataWithSQL.sql
+++ Section19/QueryingDataWithSQL/QueryingDataWithSQL.sql
@@ -16,3 +16,21 @@ INSERT INTO artists VALUES (201, "Beyonce");
 DELETE FROM artists WHERE name = 'Beyonce';
 
 INSERT INTO artists (name) VALUES ("Beyonce");
+
+SELECT * FROM artists ORDER BY name;
+SELECT * FROM albums ORDER BY name;
+SELECT * FROM albums ORDER BY name DESC;
+
+SELECT * FROM albums ORDER BY artist, name;
+
+SELECT * FROM songs ORDER BY album, track;
+
+SELECT * FROM albums WHERE _id = 439;
+SELECT * FROM artists WHERE _id = 133;
+
+SELECT songs.track, songs.title, albums.name FROM songs JOIN albums ON songs.album = albums._id;
+SELECT songs.track, songs.title, albums.name FROM songs INNER JOIN albums ON songs.album = albums._id;
+
+SELECT songs.track, songs.title, albums.name 
+FROM songs INNER JOIN albums ON songs.album = albums._id
+ORDER BY albums.name, songs.track;
